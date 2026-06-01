906d71f514f49137d193a937a4b68899223e4d54
VivekKShetty <vivek2929shetty@gmail.com>
2026-06-01 05:03:47 +0000

    comp list

--- Day-02/List.py
+++ Day-02/List.py
@@ -1,1 +1,12 @@
-my_list = [1,2,3]
+my_list = [1,2,3,3]   #duplicate values are allowed, uses []
+print(my_list[0])
+print(my_list)        #ordered output
+my_list[1] = 4        #mutable...ie changes are allowed
+print(my_list)
+
+
+
+# output 
+# 1
+# [1, 2, 3, 3]
+# [1, 4, 3, 3]
