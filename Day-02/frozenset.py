7ab13c6a4835e9e616f1b7976cec48a70a2b3aa0
VivekKShetty <vivek2929shetty@gmail.com>
2026-06-01 05:31:03 +0000

    update frozed

--- Day-02/frozenset.py
+++ Day-02/frozenset.py
@@ -1,1 +1,8 @@
+numbers = frozenset([1,2,3,4,5,6,7,8,8])  #unordered, no duplicates, imutables
+print(numbers)
+numbers.add(10)
 
+
+# output
+# {1, 2, 3, 4, 5, 6, 7, 8, 9}
+# {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
