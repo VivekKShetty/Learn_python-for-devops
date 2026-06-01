054880a574e71882fa2130ecd1220098746d5c03
VivekKShetty <vivek2929shetty@gmail.com>
2026-06-01 05:25:29 +0000

    Update set

--- Day-02/set.py
+++ Day-02/set.py
@@ -1,1 +1,9 @@
+my_set = {1,2,3,4,5,6,7,8,8,9}    #unordered, mutable
+print(my_set)
+#my_set[1]=10                      #indexing is not allowed
+my_set.add(10)
+print(my_set)
 
+# #output          #duplicates are not allowed
+# {1, 2, 3, 4, 5, 6, 7, 8, 9}
+# {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
