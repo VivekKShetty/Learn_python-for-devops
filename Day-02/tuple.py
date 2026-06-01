15de56ab23f29870fed1cde89046a3e5f6982bea
VivekKShetty <vivek2929shetty@gmail.com>
2026-06-01 05:08:49 +0000

    tuple contents

--- Day-02/tuple.py
+++ Day-02/tuple.py
@@ -1,1 +1,12 @@
+my_tuple = (1,2,3,4,5,6,7,8,8)    #ordered, allows duplicate, Immutable and uses ()
+print(my_tuple)
+my_tuple[1] = 9
 
+
+# output
+# (1, 2, 3, 4, 5, 6, 7, 8, 8)
+# Traceback (most recent call last):
+#   File "/workspaces/Learn_python-for-devops/Day-02/tuple.py", line 3, in <module>
+#     my_tuple[1] = 9
+#     ~~~~~~~~^^^
+# TypeError: 'tuple' object does not support item assignment
