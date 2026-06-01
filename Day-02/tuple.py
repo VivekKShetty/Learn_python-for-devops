c1666f1dd284a8ea3c7615aeb378a8df58aae435
VivekKShetty <vivek2929shetty@gmail.com>
2026-06-01 05:11:50 +0000

    update tuple

--- Day-02/tuple.py
+++ Day-02/tuple.py
@@ -1,6 +1,7 @@
 my_tuple = (1,2,3,4,5,6,7,8,8)    #ordered, allows duplicate, Immutable and uses ()
 print(my_tuple)
-my_tuple[1] = 9
+my_tuple[1] = 9                    #immutable
+my_tuple.append(10)                #immutable
 
 
 # output
